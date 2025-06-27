f2dd23b818027d9158e4ba3d4381fbfcd8bd7a34
Abhitosh Kumar <kumarabhitosh678@gmail.com>
2025-06-28 02:20:03 +0530

    Auto commit message: e2bba582-338f-4a63-a489-19d2446f565d

--- project-root/ContactApi/Program.cs
+++ project-root/ContactApi/Program.cs
@@ -1,1 +1,24 @@
-program.cs file content
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using ContactApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("YourConnectionString"));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
+}
+
+app.MapControllers();
+
+app.Run();
