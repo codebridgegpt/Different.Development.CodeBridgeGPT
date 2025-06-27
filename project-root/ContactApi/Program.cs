945977a7d8240024f340e26f2fa0387e06ad4024
Abhitosh Kumar <kumarabhitosh678@gmail.com>
2025-06-28 03:49:36 +0530

    Auto commit message: d6366f47-7d47-4553-96fa-1962a6f0bde7

--- project-root/ContactApi/Program.cs
+++ project-root/ContactApi/Program.cs
@@ -1,23 +1,28 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using ContactApi.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connection));
 
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("YourConnectionString"));
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-    });
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Contact API"));
 }
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
 
 app.MapControllers();
 
